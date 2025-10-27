dcb77f6dd368b02ee437ae208d197530988c7637
SeemsFishy <hddhuran@gmail.com>
2025-10-27 17:41:48 +0300

    entrypoint.sh fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ set -e
 
 /usr/sbin/iptables -t nat -F
 /usr/sbin/iptables -t nat -X
-/usr/sbin/iptables -t nat -A PREROUTING -m addrtype ! --dst-type LOCAL ! --src-type LOCAL -i $(ip a | awk '/inet.*global/ {print $NF}' | grep -v tun) -p tcp -j REDIRECT --to-port 12
+/usr/sbin/iptables -t nat -A PREROUTING -m addrtype ! --dst-type LOCAL ! --src-type LOCAL -i $(ip a | awk '/inet.*global/ {print $NF}' | grep -v tun) -p tcp -j REDIRECT --to-port 12345
 
 exec sing-box "$@"
